90496f92d574b7e7d81562a1e09d51b0b0f783b4
daria135711 <dashapaul2008@gmail.com>
2026-04-28 14:17:45 +0300

    ..

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/daria/Рабочий стол/StudyRoutine/StudyRoutine/venv'
+set -gx VIRTUAL_ENV '/home/daria/Рабочий стол/StudyRoutine/StudyRoutine_progect/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
